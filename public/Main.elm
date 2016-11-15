50f9d0bc13c0a93d923bbc6d0b3a1dcd7981ab54
Patrick Oyarzun <patrickoyarzun@gmail.com>
2016-11-15 15:50:49 -0500

    Streaming tweets onto the screen

--- public/Main.elm
+++ public/Main.elm
@@ -2,43 +2,76 @@ module Main exposing (..)
 
 import Html.App as App
 import Html
+import WebSocket
+import Navigation
+
+
+type alias Tweet =
+    String
 
 
 type alias Model =
-    {}
+    { location : Navigation.Location
+    , tweets : List Tweet
+    }
 
 
 type Msg
-    = NoOp
+    = NewLocation Navigation.Location
+    | NewTweet Tweet
 
 
 main : Program Never
 main =
-    App.program
-        { init = ( model, Cmd.none )
+    Navigation.program
+        (Navigation.makeParser identity)
+        { init = model
         , view = view
         , update = update
+        , urlUpdate = urlUpdate
         , subscriptions = subscriptions
         }
 
 
-model : Model
-model =
-    {}
+model : Navigation.Location -> ( Model, Cmd Msg )
+model loc =
+    ( { location = loc, tweets = [] }, Cmd.none )
 
 
 view : Model -> Html.Html Msg
 view model =
-    Html.text "Hello from elm!"
+    Html.div [] (List.map viewTweet model.tweets)
+
+
+viewTweet : Tweet -> Html.Html Msg
+viewTweet tweet =
+    Html.text tweet
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    let
+        url =
+            "ws://" ++ model.location.host ++ "/tweets"
+    in
+        WebSocket.listen url NewTweet
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        NoOp ->
+        NewLocation location ->
             ( model, Cmd.none )
+
+        NewTweet tweet ->
+            let
+                newTweets =
+                    (tweet :: model.tweets)
+                        |> List.take 10
+            in
+                ( { model | tweets = newTweets }, Cmd.none )
+
+
+urlUpdate : Navigation.Location -> Model -> ( Model, Cmd Msg )
+urlUpdate loc model =
+    ( { model | location = loc }, Cmd.none )
